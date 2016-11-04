202b16c4f7df6f117e5858bd35745b2777c8049e
chriswebb09 <christopher.webb@flatironschool.com>
2016-11-04 11:46:42 -0400

    new code added

---
+++ MovieSee/MovieDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailViewController.swift
+//  MovieSee
+//
+//  Created by Christopher Webb-Orenstein on 11/4/16.
+//  Copyright © 2016 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var moviePoster: UIImageView!
+    
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+}
--- MovieSee/MovieViewController.swift
+++ MovieSee/MovieViewController.swift
@@ -164,3 +164,29 @@ extension MovieViewController {
     }
 }
 
+
+extension MovieViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
