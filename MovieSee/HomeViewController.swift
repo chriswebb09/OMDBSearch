ac4501324a04617fe3fe6adbf0c11aad91bdc5af
chriswebb09 <christopher.webb@flatironschool.com>
2016-11-05 13:42:19 -0400

    new code added

--- MovieSee/HomeViewController.swift
+++ MovieSee/HomeViewController.swift
@@ -12,16 +12,13 @@ import UIKit
 final class HomeViewController: UICollectionViewController {
     
     let store = MovieDataStore.sharedInstance
-    
     let shared = OMDBClient.sharedInstance
-    
+    var searchURL = ""
     let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
     
     @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var searchButtonItem: UIBarButtonItem!
-    
-    var searchURL = ""
     
     fileprivate let reuseIdentifier = "MovieCell"
     fileprivate let sectionInsets = UIEdgeInsets(top: 40.0, left: 30.0, bottom: 50.0, right: 30.0)
@@ -35,14 +32,16 @@ final class HomeViewController: UICollectionViewController {
 extension HomeViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.collectionView?.delegate = self
+        self.collectionView?.dataSource = self
         self.searchButton.addTarget(self, action: #selector(downloadFromAPI), for: .touchUpInside)
-        //        activityIndicator.hidesWhenStopped = true
     }
     
     func downloadFromAPI() {
+        
         self.searchURL.removeAll()
         self.searches.removeAll()
+        
         let searchTerms = searchField.text?.components(separatedBy: " ")
         if (searchField.text?.characters.count)! > 0 {
             for term in searchTerms! {
@@ -52,13 +51,10 @@ extension HomeViewController {
                     searchURL = term
                 }
             }
-            
             shared.searchAPI(withURL: searchURL, terms: searchURL,  handler: { result in
                 self.store.searchResults.append(result!)
-                
                 print(self.store.searchResults)
                 self.store.searchResults.forEach { mov in
-                    
                     DispatchQueue.main.async {
                         self.store.movieArray = mov.searchResults
                     }
@@ -68,8 +64,6 @@ extension HomeViewController {
                 }
             })
             
-            self.collectionView?.delegate = self
-            self.collectionView?.dataSource = self
             self.searchField.text = nil
             self.activityIndicator.stopAnimating()
             self.activityIndicator.hidesWhenStopped = true
@@ -82,7 +76,6 @@ private extension HomeViewController {
         return self.store.movieArray[(indexPath as NSIndexPath).row]
     }
 }
-
 // MARK: - UICollectionViewDataSource
 extension HomeViewController {
     
